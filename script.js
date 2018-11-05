31af7586cc8115c2592e29d7de089e5e59718b43
WebDeveloperTom <tom@webdevelopertom.com>
2018-11-06 10:04:19 +1300

    added comments and error display

--- script.js
+++ script.js
@@ -57,18 +57,27 @@ const questions = [
     answer: "3"
   }
 ];
-
-//rating system, show correct answers that was wrong.
+//TODO
+//rating system.
 //login form/form validation -> name email password
+// restart quiz button
+//progress bar?
+//if highscore is beaten, display a congratz message
 
+//Keeping track of the current question count
 let questionCount = 0;
+//The users current score.
 let userScore = 0;
+// The object containing all the questions the user has got wrong.
 let questionsWrong = [];
-document.getElementById("score").innerText = userScore;
+//The users highest quiz score for this session.
+let userHighScore = 0;
 
 function startQuiz() {
+  //hide the start quiz button and display the quiz
   document.querySelector(".quiz-start").style.display = "none";
   document.querySelector("#quiz-container").style.display = "block";
+  //fetches current question and displays
   displayQandA(questionCount);
 }
 
@@ -81,38 +90,67 @@ function displayResults() {
   //display questionsWrong arr.
   let wrongQuestionList = "";
   questionsWrong.forEach(function(wrongItem, index) {
+    //destructuring the wrongItem object
     let { qNumber, userChoice } = wrongItem;
+    //grabbing the correct answer from the orginal question array
     let correctAns = questions[qNumber].answer;
+    //creating the HTML to display
     wrongQuestionList += `<div>
-      <p>${questions[qNumber].question}</p>
-      <p>You chose: ${questions[qNumber].options[userChoice - 1]} </p>
-      <p>The correct answer was ${
+      <p>Q${qNumber + 1}. ${questions[qNumber].question}</p>
+      <p>You chose: <span class="wrongAns" style="color:red;">${
+        questions[qNumber].options[userChoice - 1]
+      }</span></p>
+      <p>The correct answer was <span class="rightAns" style="color:green;">${
         questions[qNumber].options[correctAns - 1]
-      }</p>
+      }</span></p>
     </div>`;
   });
+  //sending the HTML to be displayed
   document.querySelector(".correctResults").innerHTML = wrongQuestionList;
-
   //display user Score
+  document.getElementById("userScore").innerText = `You scored ${userScore}!`;
   //display grade.
+  displayUserGrade(userScore);
+  //highscore
+  checkHighscore(userScore);
   //try again button
 }
 
+//NEED TO FINISH
+function displayUserGrade(x) {
+  let grade = "";
+  if (x <= 3) {
+    grade = "You need to do some study. Please try again";
+  } else if (3 < x <= 6) {
+    grade = "There's room for improvement here. Keep at it!";
+  }
+}
+
 function updateQandA() {
+  //remove any warning responses
+  document.getElementById("response").innerHTML = "";
+  //increase the question count
   questionCount++;
+  //check to see if we're going to be on the final question
   if (questionCount == 9) {
+    //update the button
     document.getElementById("submit").innerText = "Submit and get results";
   }
+  //check to see if there are no more questions
   if (questionCount == 10) {
     displayResults();
     return;
   }
+  //otherwise display the next question
   displayQandA(questionCount);
 }
 
 function checkAnswer() {
+  //grab the correct answer from the question array
   let correctAns = questions[questionCount].answer;
+  //grabbing any checked radio buttons
   let userAns = document.querySelector('input[name="options"]:checked');
+  //checking to see if there ARE any checked radio buttons
   if (!userAns) {
     //if the user has not selected an answer.
     //display error message;
@@ -120,29 +158,41 @@ function checkAnswer() {
       "<p class='warning'>Please select an option</p>";
     return;
   }
-
+  //if the user answer doesnt match the correct answer
   if (userAns.value !== correctAns) {
+    //get the question number and users selection and create an object
     const wrongQuestion = {
       qNumber: questionCount,
       userChoice: userAns.value
     };
+    //push the object into an array for use in the result screen
     questionsWrong.push(wrongQuestion);
+    //update the question
     updateQandA();
   } else {
+    //answer is correct, increase the score
     userScore++;
-    document.getElementById("score").innerText = userScore;
+    // document.getElementById("score").innerText = userScore;
+    //update the question
     updateQandA();
   }
 }
 
 function displayQandA(x) {
-  document.querySelector(".question").innerText = `${questionCount + 1}. ${
+  //takes in a value and fetches and display the value's question and choices.
+  document.querySelector(".question").innerText = `${x + 1}. ${
     questions[x].question
   }`;
   let choices = "";
+  //loops over each answer in the array and creates a radio button for each
   questions[x].options.forEach(function(option, index) {
     choices += `<input type='radio' name='options' value="${index +
       1}">${option}<br>`;
   });
   document.getElementById("answerList").innerHTML = choices;
+}
+
+function checkHighscore(userScore) {
+  //condition ? true : false
+  userScore > userHighScore ? (userHighScore = userScore) : null;
 }
